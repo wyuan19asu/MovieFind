78e61433ee6dca067576ad03767571d32446322d
wyuan19asu <wyuan19@asu.edu>
2022-11-11 01:33:36 -0700

    some css for navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import navLogo from '../assets/MovieFind.png'
 
 export default function Nav() {
     return (
         <div className='nav__bar'>
-            <figure className="nav__logo">
-                <img src="" alt="" className="nav__logo--img" />
-            </figure>
+            <div className="nav__bar--items">
+                <figure className="nav__logo">
+                    <img src={navLogo} alt="" className="nav__logo--img" />
+                </figure>
+
+                <ul className="nav__link--items">
+                    <li className="nav__link--item">
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav__link--item">
+                        Find Movies
+                    </li>
+                    <li className="nav__link--item">
+                        Contact
+                    </li>
+                </ul>
+
+            </div>
         </div>
     )
 }
