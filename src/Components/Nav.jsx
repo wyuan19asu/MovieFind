403c7303fb8df3ad89a9d0737b69b12b805c92e4
wyuan19asu <wyuan19@asu.edu>
2022-11-11 00:31:30 -0700

    initial commit

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <div className='nav__bar'>
+            <figure className="nav__logo">
+                <img src="" alt="" className="nav__logo--img" />
+            </figure>
+        </div>
+    )
+}
