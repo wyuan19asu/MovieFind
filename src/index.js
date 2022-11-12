5a5d52553ca9aa5a9209c6a4e59978d1c2eee27b
wyuan19asu <wyuan19@asu.edu>
2022-11-11 19:44:23 -0700

    fontawesomeicons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faMagnifyingGlass);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
