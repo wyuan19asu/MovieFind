10562307e2716bc7699acb79a6092d22975967be
wyuan19asu <wyuan19@asu.edu>
2022-11-11 19:45:43 -0700

    remove div

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,26 +5,27 @@ import navLogo from '../assets/MovieFind.png'
 export default function Nav() {
     return (
         <div className='nav__bar'>
-            <div className="nav__bar--items">
-                <figure className="nav__logo">
-                    <img src={navLogo} alt="" className="nav__logo--img" />
-                </figure>
+            <figure className="nav__logo">
+                <img src={navLogo} alt="" className="nav__logo--img" />
+            </figure>
 
-                <ul className="nav__link--items">
-                    <li className="nav__link--item">
-                        <Link to="/">
-                            Home
-                        </Link>
-                    </li>
-                    <li className="nav__link--item">
+            <ul className="nav__link--items">
+                <li className="nav__link--item">
+                    <Link to="/" className='nav__link nav__link-hover'>
+                        Home
+                    </Link>
+                </li>
+                <li className="nav__link--item">
+                    <Link to="/findmovies" className='nav__link nav__link-hover'>
                         Find Movies
-                    </li>
-                    <li className="nav__link--item">
+                    </Link>
+                </li>
+                <li className="nav__link--item">
+                    <a href="mailto:yuanweicheng1@gmail.com" className="nav__link--item-primary">
                         Contact
-                    </li>
-                </ul>
-
-            </div>
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
