1b79710320b9b6658db3110a1881762842ffca32
wyuan19asu <wyuan19@asu.edu>
2022-11-11 22:57:10 -0700

    added link to home for logo

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -4,28 +4,31 @@ import navLogo from '../assets/MovieFind.png'
 
 export default function Nav() {
     return (
-        <div className='nav__bar'>
-            <figure className="nav__logo">
-                <img src={navLogo} alt="" className="nav__logo--img" />
-            </figure>
-
-            <ul className="nav__link--items">
-                <li className="nav__link--item">
-                    <Link to="/" className='nav__link nav__link-hover'>
-                        Home
-                    </Link>
-                </li>
-                <li className="nav__link--item">
-                    <Link to="/findmovies" className='nav__link nav__link-hover'>
-                        Find Movies
-                    </Link>
-                </li>
-                <li className="nav__link--item">
-                    <a href="mailto:yuanweicheng1@gmail.com" className="nav__link--item-primary">
-                        Contact
-                    </a>
-                </li>
-            </ul>
-        </div>
+        <nav>
+            <div className='nav__bar'>
+                <Link to="/" className='nav__link nav__link-hover'>
+                    <figure className="nav__logo">
+                        <img src={navLogo} alt="" className="nav__logo--img" />
+                    </figure>
+                </Link>
+                <ul className="nav__link--items">
+                    <li className="nav__link--item">
+                        <Link to="/" className='nav__link nav__link-hover'>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav__link--item">
+                        <Link to="/findmovies" className='nav__link nav__link-hover'>
+                            Find Movies
+                        </Link>
+                    </li>
+                    <li className="nav__link--item">
+                        <a href="mailto:yuanweicheng1@gmail.com" className="nav__link--item-primary">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
