f976886efe301290ca77df2d2cc2e23ec4b57ddc
wyuan19asu <wyuan19@asu.edu>
2022-11-11 22:56:37 -0700

    updated component for searchbar

--- src/Components/HomeContent.jsx
+++ src/Components/HomeContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import homeImg from '../assets/undraw_home_cinema_l7yl.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import SearchBar from './SearchBar'
 
 export default function HomeContent() {
     return (
@@ -11,12 +12,7 @@ export default function HomeContent() {
             <h2 className="home__subtitle">
                 Find your movies with <span className="purple">MovieFind</span>
             </h2>
-            <div className="search__bar">
-                <input type="text" className="search__bar--input" />
-                <button className="search__button">
-                    <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" className='search__icon' />
-                </button>
-            </div>
+            <SearchBar />
             <figure className="home__image--container">
                 <img src={homeImg} alt="" className="home__image" />
             </figure>
