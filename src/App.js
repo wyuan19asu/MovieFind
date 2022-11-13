81cd4668ce6ba245867827091651822dad895fef
wyuan19asu <wyuan19@asu.edu>
2022-11-13 15:05:28 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import {
 import FindMovies from "./Pages/FindMovies";
 import Home from "./Pages/Home";
 import NewFindMovies from "./Pages/NewFindMovies"
+import Nav from './Components/Nav';
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/NewFindMovies/:term" element={<NewFindMovies />}></Route>
           <Route path="/findmovies/" element={<FindMovies />}></Route>
         </Routes>
-
+        <Footer />
       </div>
     </Router>
   );
