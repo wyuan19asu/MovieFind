cd18d9ffdfce2aa4bb41b41d6a7d5588608badd8
wyuan19asu <wyuan19@asu.edu>
2022-11-18 12:13:51 -0700

    removed test fn call

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -28,7 +28,6 @@ export function MovieModal({ addtoFave, favorite, show, onHide, movieid }) {
     function movieExistsinFave() {
         return favorite.find((movie) => movie.imdbID === movieItem.imdbID);
     }
-    movieExistsinFave();
     return (
         <>
             <Modal
